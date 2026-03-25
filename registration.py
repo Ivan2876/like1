6febc72cdc8f19f7bc250e1d11017e777d3d045b
“John <john@email.com>
2026-03-25 16:26:24 +0200

    good changs

--- registration.py
+++ registration.py
@@ -3,21 +3,21 @@ import messages
 name_input = input(messages.MSG_INPUT_NAME)
 name = name_input.strip()
 if name.isalpha():
-    print(f"{messages.MSG_NAME_OK}: {name.title()}")
+    print(messages.MSG_NAME_OK.format(name=name))
 else:
     print(messages.MSG_NAME_ERROR)
 
 age_input = input(messages.MSG_INPUT_AGE)
 age = age_input.strip().lstrip("0")
 if age.isdigit():
-    print(f"{messages.MSG_AGE_OK}: {age}")
+    print(messages.MSG_AGE_OK.format(age=age))
 else:
     print(messages.MSG_AGE_ERROR)
 
 phone_input = input(messages.MSG_INPUT_PHONE)
 phone = phone_input.strip()
 if phone.isdigit():
-    print(f"{messages.MSG_PHONE_OK}:{phone}")
+    print(messages.MSG_PHONE_OK.format(phone=phone))
     print(messages.MSG_FINISH)
 else:
     print(messages.MSG_PHONE_ERROR)
