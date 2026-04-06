47ce006748c7ee55bc96ea0ad4498dfc51741939
“John <john@email.com>
2026-04-06 19:00:25 +0300

    good changs

---
+++ hw_lists_loops.py
@@ -0,0 +1,18 @@
+#task1
+list = [1, 5, 2, 8, 3, 7]
+numbers = list
+largest_number_lenght = 0
+smallest_number_lenght = 0
+
+for number in numbers:
+    print(list)
+    current_number_lenght = int(number)
+
+
+
+
+
+
+
+print(f"{largest_number_lenght=}")
+print(f"{smallest_number_lenght=}")
