627ef867bfcaf2e0eaddf18db108293dd4b05a17
“John <john@email.com>
2026-04-27 21:22:17 +0300

    good changs

---
+++ functions.py
@@ -0,0 +1,19 @@
+def calculate_summa(number_1: int | float, number_2: int | float) -> float:
+    result = number_1 + number_2
+
+    return result * 1.0
+
+
+result_calculate_summa = calculate_summa(number_1=1, number_2=9.6)
+
+print(result_calculate_summa)
+
+mult = result_calculate_summa * 20
+
+print(mult)
+
+
+
+
+
+
