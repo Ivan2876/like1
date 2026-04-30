3ee201b68833e7e1f88084ae86b152e88e996bed
“John <john@email.com>
2026-04-30 16:08:10 +0300

    good changs

--- functions.py
+++ functions.py
@@ -1,17 +1,22 @@
-def calculate_summa(number_1: int | float, number_2: int | float) -> float:
-    result = number_1 + number_2
+from utils import get_distance, calculate_summa, get_number_5, print_msg
 
-    return result * 1.0
+print_msg()
 
+
+number_5 = get_number_5()
+s = number_5 + 5
+print(f"{number_5=}")
 
 result_calculate_summa = calculate_summa(number_1=1, number_2=9.6)
-
 print(result_calculate_summa)
-
 mult = result_calculate_summa * 20
-
 print(mult)
 
+distance1 = get_distance(time_seconds=2, velocity_meters_per_second=2)
+print(distance1)
+
+distance2 = get_distance(time_seconds=2.522, velocity_meters_per_second=297.875765)
+print(distance2)
 
 
 
