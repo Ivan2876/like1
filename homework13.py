3d7958af0241d990443019b9a4a74c3ad429c694
“John <john@email.com>
2026-05-06 16:00:50 +0300

    good changs

---
+++ homework13.py
@@ -0,0 +1,9 @@
+from homework13utils import get_sume, get_accepts_string, get_list_numbers_as_string
+
+function1 = get_sume(8,4)
+print(function1)
+function2 = get_accepts_string(line = 'hello',)
+print(function2)
+list_of_numbers_as_string, *dict = "11,26,32", ","
+print(list_of_numbers_as_string)
+print(*dict)
